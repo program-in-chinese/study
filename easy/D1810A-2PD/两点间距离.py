f2459423a668eabd815192e8db3fdea21b491b1c
劉東熠 <absente@live.cn>
2018-10-23 21:05:59 +0800

    Update 两点间距离.py

--- easy/D1810A-2PD/两点间距离.py
+++ easy/D1810A-2PD/两点间距离.py
@@ -2,7 +2,7 @@ class 坐标:
     def __init__(此,横,纵):
         此.x, 此.y = 横,纵
 
-    def __repr__(i):  # 显示坐标; 例： print 坐标(1,1)
+    def __repr__(i):  # 显示坐标; 例： print (坐标(1,1))
         print(
             "X="+i.x+" Y="+i.y)
 
@@ -14,7 +14,9 @@ def 计算(A:坐标,B:坐标):  # 这里用了py3.5+的type hint
     return 距离
 
 def 解():
-    结果 = 计算(坐标(1,2),坐标(5,7))
+    p1,p2 = 坐标(1,2),坐标(5,7)
+    结果 = 计算(p1,p2)
+    print(p1)
     print(结果)
 
 if __name__ == '__main__':
