2963dde3dedb7ebe02423577320f96786ed50998
fromwheretowhere <fromwheretowhere.service@gmail.com>
2019-02-09 19:19:38 -0800

    直接字符串替换

---
+++ 1-基础/控制台/README.md
@@ -0,0 +1,14 @@
+运行效果如下:
+```
+$ python3 解释器.py
+zhpy in darwin on top of Python 3.6.5
+>>> print(学)
+Traceback (most recent call last):
+  File "<console>", line 1, in <module>
+命名错误: 命名'学'未定义
+>>> print(名)
+Traceback (most recent call last):
+  File "<console>", line 1, in <module>
+命名错误: 命名'名'未定义
+>>> exit()
+```
--- 1-基础/控制台/解释器.py
+++ 1-基础/控制台/解释器.py
@@ -1,10 +1,16 @@
 from code import InteractiveConsole
 import sys
+import traceback
 
 class ZhPyConsole(InteractiveConsole):
     """
     Wrapper around Python and filter input/output to the shell
     """
+    字典 = {
+        " is not defined": "未定义",
+        "NameError": "命名错误",
+        "name ": "命名"
+        }
 
     def push(self, line):
         self.buffer.append(line)
@@ -14,6 +20,25 @@ class ZhPyConsole(InteractiveConsole):
         if not more:
             self.resetbuffer()
         return more
+
+    def showtraceback(self):
+        sys.last_type, sys.last_value, last_tb = ei = sys.exc_info()
+        sys.last_traceback = last_tb
+        try:
+            行 = traceback.format_exception(ei[0], ei[1], last_tb.tb_next)
+            汉化行 = []
+            if sys.excepthook is sys.__excepthook__:
+                for 某行 in 行:
+                    for 英文 in self.字典:
+                        某行 = 某行.replace(英文, self.字典[英文])
+                    汉化行.append(某行)
+                self.write(''.join(汉化行))
+            else:
+                # If someone has set sys.excepthook, we let that take precedence
+                # over self.write
+                sys.excepthook(ei[0], ei[1], last_tb)
+        finally:
+            last_tb = ei = None
 
 def interpreter(lang=None):
     """
