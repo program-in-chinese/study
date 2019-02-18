a944cd33a7fa7944cf19176bbfe033c2f6bd4548
fromwheretowhere <fromwheretowhere.service@gmail.com>
2019-02-18 15:29:04 -0800

    通过现有测试

--- 1-基础/控制台/test翻译.py
+++ 1-基础/控制台/test翻译.py
@@ -10,13 +10,13 @@ class test功能(unittest.TestCase):
         self.assertEqual(self.控制台.中文化("NameError: name '学' is not defined"), "命名错误: 命名'学'未定义")
         self.assertEqual(self.控制台.中文化("SyntaxError: invalid syntax"), "语法错误: 不正确的语法")
         self.assertEqual(self.控制台.中文化("ZeroDivisionError: division by zero"), "除零错误: 不能被0除")
-        # self.assertEqual(self.控制台.中文化("NameError: free variable 'type' referenced before assignment in enclosing scope"), "命名错误: 在闭合作用域中, 自由变量'type'在引用之前未被赋值")
-        # self.assertEqual(self.控制台.中文化("UnboundLocalError: local variable '学生' referenced before assignment"), "本地变量未定义错误: 本地变量'学生'在引用之前未被赋值")
+        self.assertEqual(self.控制台.中文化("NameError: free variable 'type' referenced before assignment in enclosing scope"), "命名错误: 在闭合作用域中, 自由变量'type'在引用之前未被赋值")
+        self.assertEqual(self.控制台.中文化("UnboundLocalError: local variable '学生' referenced before assignment"), "本地变量未定义错误: 本地变量'学生'在引用之前未被赋值")
         self.assertEqual(self.控制台.中文化("TypeError: must be str, not int"), "类型错误: 不能将整数自动转换为字符串")
-        # self.assertEqual(self.控制台.中文化("TypeError: unsupported operand type(s) for /: 'str' and 'str'"), "类型错误: 不支持/操作数: 字符串和字符串")
-        # self.assertEqual(self.控制台.中文化("TypeError: unsupported operand type(s) for ** or pow(): 'str' and 'int'"),"类型错误: 不支持**或pow()的操作数: 字符串和整数"
-        # self.assertEqual(self.控制台.中文化("TypeError: can't multiply sequence by non-int of type 'str'"), "类型错误: 不能用非整数的类型--字符串对序列进行累乘")
-        self.assertEqual(self.控制台.中文化("AttributeError: 'list' object has no attribute 'length'"), "属性错误: 'list'个体没有属性'length'")
+        self.assertEqual(self.控制台.中文化("TypeError: unsupported operand type(s) for /: 'str' and 'str'"), "类型错误: 不支持/的操作数: 'str'和'str'")
+        self.assertEqual(self.控制台.中文化("TypeError: unsupported operand type(s) for ** or pow(): 'str' and 'int'"), "类型错误: 不支持**或pow()的操作数: 'str'和'int'")
+        self.assertEqual(self.控制台.中文化("TypeError: can't multiply sequence by non-int of type 'str'"), "类型错误: 不能用非整数的类型--'str'对序列进行累乘")
+        self.assertEqual(self.控制台.中文化("AttributeError: 'list' object has no attribute 'length'"), "属性错误: 'list'个体没有'length'属性")
 
 if __name__ == '__main__':
     unittest.main()
--- 1-基础/控制台/解释器.py
+++ 1-基础/控制台/解释器.py
@@ -1,4 +1,5 @@
 from code import InteractiveConsole
+import re
 import sys
 import traceback
 
@@ -10,12 +11,7 @@ class ZhPyConsole(InteractiveConsole):
         "Traceback (most recent call last):": "回溯 (最近的调用在最后):",
         "SyntaxError: invalid syntax": "语法错误: 不正确的语法",
         "ZeroDivisionError: division by zero": "除零错误: 不能被0除",
-        "TypeError: must be str, not int": "类型错误: 不能将整数自动转换为字符串",
-        "AttributeError": "属性错误",
-        " object has no attribute ": "个体没有属性",
-        " is not defined": "未定义",
-        "NameError": "命名错误",
-        "name ": "命名"
+        "TypeError: must be str, not int": "类型错误: 不能将整数自动转换为字符串"
         }
 
     def showtraceback(self):
@@ -38,9 +34,26 @@ class ZhPyConsole(InteractiveConsole):
     def 中文化(self, 原始信息):
         if 原始信息 in self.字典:
             return self.字典[原始信息]
-        for 英文 in self.字典:
-            原始信息 = 原始信息.replace(英文, self.字典[英文])
-        return 原始信息
+        return self.模式替换(原始信息)
+
+    # 参考: https://docs.python.org/3/library/re.html#re.sub
+    def 模式替换(self, 原始信息):
+        if re.match(r"NameError: name '(.*)' is not defined", 原始信息):
+            return re.sub(r"NameError: name '(.*)' is not defined", r"命名错误: 命名'\1'未定义", 原始信息)
+        elif re.match(r"AttributeError: '(.*)' object has no attribute '(.*)'", 原始信息):
+            return re.sub(r"AttributeError: '(.*)' object has no attribute '(.*)'", r"属性错误: '\1'个体没有'\2'属性", 原始信息)
+        elif re.match(r"NameError: free variable '(.*)' referenced before assignment in enclosing scope", 原始信息):
+            return re.sub(r"NameError: free variable '(.*)' referenced before assignment in enclosing scope", r"命名错误: 在闭合作用域中, 自由变量'\1'在引用之前未被赋值", 原始信息)
+        elif re.match(r"UnboundLocalError: local variable '(.*)' referenced before assignment", 原始信息):
+            return re.sub(r"UnboundLocalError: local variable '(.*)' referenced before assignment", r"本地变量未定义错误: 本地变量'\1'在引用之前未被赋值", 原始信息)
+        elif re.match(r"TypeError: unsupported operand type\(s\) for /: '(.*)' and '(.*)'", 原始信息):
+            return re.sub(r"TypeError: unsupported operand type\(s\) for /: '(.*)' and '(.*)'", r"类型错误: 不支持/的操作数: '\1'和'\2'", 原始信息)
+        elif re.match(r"TypeError: unsupported operand type\(s\) for \*\* or pow\(\): '(.*)' and '(.*)'", 原始信息):
+            return re.sub(r"TypeError: unsupported operand type\(s\) for \*\* or pow\(\): '(.*)' and '(.*)'", r"类型错误: 不支持**或pow()的操作数: '\1'和'\2'", 原始信息)
+        elif re.match(r"TypeError: can't multiply sequence by non-int of type '(.*)'", 原始信息):
+            return re.sub(r"TypeError: can't multiply sequence by non-int of type '(.*)'", r"类型错误: 不能用非整数的类型--'\1'对序列进行累乘", 原始信息)
+        else:
+            return 原始信息
 
 def 解释器(lang=None):
     """
