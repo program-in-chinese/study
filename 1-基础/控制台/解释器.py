66dad7c5ca0a6887874765dc94d4ccd6d3c15100
fromwheretowhere <fromwheretowhere.service@gmail.com>
2019-02-19 20:50:47 -0800

    重构: 避免重复代码

--- 1-基础/控制台/解释器.py
+++ 1-基础/控制台/解释器.py
@@ -7,6 +7,22 @@ class 中文报错控制台(InteractiveConsole):
     """
     封装Python控制台, 对输出进行转换
     """
+    字典 = {
+        r"Traceback \(most recent call last\):": r"回溯 (最近的调用在最后):",
+        r"AttributeError: '(.*)' object has no attribute '(.*)'": r"属性错误: '\1'个体没有'\2'属性",
+        r"NameError: name '(.*)' is not defined": r"命名错误: 命名'\1'未定义",
+        r"NameError: free variable '(.*)' referenced before assignment in enclosing scope": r"命名错误: 在闭合作用域中, 自由变量'\1'在引用之前未被赋值",
+        r"SyntaxError: invalid syntax": r"语法错误: 不正确的语法",
+        r"TypeError: unsupported operand type\(s\) for /: '(.*)' and '(.*)'": r"类型错误: 不支持/的操作数: '\1'和'\2'",
+        r"TypeError: unsupported operand type\(s\) for \*\* or pow\(\): '(.*)' and '(.*)'": r"类型错误: 不支持**或pow()的操作数: '\1'和'\2'",
+        r"TypeError: can't multiply sequence by non-int of type '(.*)'": r"类型错误: 不能用非整数的类型--'\1'对序列进行累乘",
+        r'TypeError: can only concatenate list \(not "(.*)"\) to list': r'类型错误: 只能将list(而非"\1")联结到list',
+        r"TypeError: must be str, not int": r"类型错误: 不能将整数自动转换为字符串",
+        r"UnboundLocalError: local variable '(.*)' referenced before assignment": r"本地变量未定义错误: 本地变量'\1'在引用之前未被赋值",
+        r"ZeroDivisionError: division by zero": r"除零错误: 不能被0除",
+        }
+
+    # 由InteractiveConsole.showsyntaxerror源码改写
     def showsyntaxerror(self, filename=None):
         type, value, tb = sys.exc_info()
         sys.last_type = type
@@ -34,6 +50,7 @@ class 中文报错控制台(InteractiveConsole):
             # over self.write
             sys.excepthook(type, value, tb)
 
+    # 由InteractiveConsole.showtraceback源码改写
     def showtraceback(self):
         sys.last_type, sys.last_value, 回溯信息 = 运行信息 = sys.exc_info()
         sys.last_traceback = 回溯信息
@@ -51,37 +68,12 @@ class 中文报错控制台(InteractiveConsole):
         finally:
             回溯信息 = 运行信息 = None
 
-    def 中文化(self, 原始信息):
-        return self.模式替换(原始信息)
-
     # 参考: https://docs.python.org/3/library/re.html#re.sub
-    def 模式替换(self, 原始信息):
-        if re.match(r"NameError: name '(.*)' is not defined", 原始信息):
-            return re.sub(r"NameError: name '(.*)' is not defined", r"命名错误: 命名'\1'未定义", 原始信息)
-        elif re.match(r"AttributeError: '(.*)' object has no attribute '(.*)'", 原始信息):
-            return re.sub(r"AttributeError: '(.*)' object has no attribute '(.*)'", r"属性错误: '\1'个体没有'\2'属性", 原始信息)
-        elif re.match(r"NameError: free variable '(.*)' referenced before assignment in enclosing scope", 原始信息):
-            return re.sub(r"NameError: free variable '(.*)' referenced before assignment in enclosing scope", r"命名错误: 在闭合作用域中, 自由变量'\1'在引用之前未被赋值", 原始信息)
-        elif re.match(r"UnboundLocalError: local variable '(.*)' referenced before assignment", 原始信息):
-            return re.sub(r"UnboundLocalError: local variable '(.*)' referenced before assignment", r"本地变量未定义错误: 本地变量'\1'在引用之前未被赋值", 原始信息)
-        elif re.match(r"TypeError: unsupported operand type\(s\) for /: '(.*)' and '(.*)'", 原始信息):
-            return re.sub(r"TypeError: unsupported operand type\(s\) for /: '(.*)' and '(.*)'", r"类型错误: 不支持/的操作数: '\1'和'\2'", 原始信息)
-        elif re.match(r"TypeError: unsupported operand type\(s\) for \*\* or pow\(\): '(.*)' and '(.*)'", 原始信息):
-            return re.sub(r"TypeError: unsupported operand type\(s\) for \*\* or pow\(\): '(.*)' and '(.*)'", r"类型错误: 不支持**或pow()的操作数: '\1'和'\2'", 原始信息)
-        elif re.match(r"TypeError: can't multiply sequence by non-int of type '(.*)'", 原始信息):
-            return re.sub(r"TypeError: can't multiply sequence by non-int of type '(.*)'", r"类型错误: 不能用非整数的类型--'\1'对序列进行累乘", 原始信息)
-        elif re.match(r'TypeError: can only concatenate list \(not "(.*)"\) to list', 原始信息):
-            return re.sub(r'TypeError: can only concatenate list \(not "(.*)"\) to list', r'类型错误: 只能将list(而非"\1")联结到list', 原始信息)
-        elif re.match(r"Traceback \(most recent call last\):", 原始信息):
-            return re.sub(r"Traceback \(most recent call last\):", r"回溯 (最近的调用在最后):", 原始信息)
-        elif re.match(r"SyntaxError: invalid syntax", 原始信息):
-            return re.sub(r"SyntaxError: invalid syntax", r"语法错误: 不正确的语法", 原始信息)
-        elif re.match(r"ZeroDivisionError: division by zero", 原始信息):
-            return re.sub(r"ZeroDivisionError: division by zero", r"除零错误: 不能被0除", 原始信息)
-        elif re.match(r"TypeError: must be str, not int", 原始信息):
-            return re.sub(r"TypeError: must be str, not int", r"类型错误: 不能将整数自动转换为字符串", 原始信息)
-        else:
-            return 原始信息
+    def 中文化(self, 原始信息):
+        for 英文模式 in self.字典:
+            if re.match(英文模式, 原始信息):
+                return re.sub(英文模式, self.字典[英文模式], 原始信息)
+        return 原始信息
 
 def 解释器(lang=None):
     """
