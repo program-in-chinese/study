12105a3d62dfb7e8952ea2026cf6341a9b1458ef
fromwheretowhere <fromwheretowhere.service@gmail.com>
2019-10-07 04:04:09 -0700

    文字合成

--- 1-基础/wav文件处理/合成.py
+++ 1-基础/wav文件处理/合成.py
@@ -16,19 +16,22 @@ def 从wav文件读取信息(文件名):
     return 信息
 
 # 参考: https://blog.csdn.net/zzZ_CMing/article/details/81739193
-def 合成(文件名):
-    帧 = []
-    格式信息1 = 从wav文件读取信息("你.wav")
-    格式信息2 = 从wav文件读取信息("好.wav")
-    帧 = 格式信息1["帧"] + 格式信息2["帧"]
+def 合成(文字):
+    帧 = b''
+    格式信息 = {}
+    for 字 in 文字:
+        # TODO: 多系统/
+        格式信息 = 从wav文件读取信息("单字/" + 字 + ".wav")
+        帧 += 格式信息["帧"]
 
-    文件 = wave.open(文件名, 'wb')
-    文件.setnchannels(格式信息1["频道数"])
-    文件.setsampwidth(格式信息1["量化位数"])
-    文件.setframerate(格式信息1["帧速"])
+    文件 = wave.open("输出/" + 文字 + ".wav", 'wb')
+    文件.setnchannels(格式信息["频道数"])
+    文件.setsampwidth(格式信息["量化位数"])
+    文件.setframerate(格式信息["帧速"])
     文件.writeframes(帧)
     文件.close()
 
     print("*"*10, "合成结束\n")
 
-合成("你好.wav")
+合成("我是中国人")
+合成("我是小学生")
