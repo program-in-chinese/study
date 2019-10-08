7397d326b4922a51247b208b07e8fbfa3d8e3210
fromwheretowhere <fromwheretowhere.service@gmail.com>
2019-10-07 23:57:13 -0700

    简化

--- 2.1-topic：数据处理/拼音表/拼音提取.py
+++ 2.1-topic：数据处理/拼音表/拼音提取.py
@@ -11,41 +11,17 @@ def 遍历表格(文件名):
     for 行 in range(开始行, 结束行):
         拼音1 = 表单.cell(row = 行, column = 1).value.lower()
 
-        拼音1入声 = 表单.cell(row = 行, column = 2).value
-        拼音1一声 = 表单.cell(row = 行, column = 3).value
-        拼音1二声 = 表单.cell(row = 行, column = 4).value
-        拼音1三声 = 表单.cell(row = 行, column = 5).value
-        拼音1四声 = 表单.cell(row = 行, column = 6).value
-
-        if 拼音1入声 != None:
-            所有拼音.append(拼音1 + '0')
-        if 拼音1一声 != None:
-            所有拼音.append(拼音1 + '1')
-        if 拼音1二声 != None:
-            所有拼音.append(拼音1 + '2')
-        if 拼音1三声 != None:
-            所有拼音.append(拼音1 + '3')
-        if 拼音1四声 != None:
-            所有拼音.append(拼音1 + '4')
+        for 列 in range(2, 7):
+            对应字 = 表单.cell(row = 行, column = 列).value
+            if 对应字 != None:
+                所有拼音.append(拼音1 + str(列 - 2))
 
         拼音2 = 表单.cell(row = 行, column = 7).value.lower()
 
-        拼音2入声 = 表单.cell(row = 行, column = 8).value
-        拼音2一声 = 表单.cell(row = 行, column = 9).value
-        拼音2二声 = 表单.cell(row = 行, column = 10).value
-        拼音2三声 = 表单.cell(row = 行, column = 11).value
-        拼音2四声 = 表单.cell(row = 行, column = 12).value
-        
-        if 拼音2入声 != None:
-            所有拼音.append(拼音2 + '0')
-        if 拼音2一声 != None:
-            所有拼音.append(拼音2 + '1')
-        if 拼音2二声 != None:
-            所有拼音.append(拼音2 + '2')
-        if 拼音2三声 != None:
-            所有拼音.append(拼音2 + '3')
-        if 拼音2四声 != None:
-            所有拼音.append(拼音2 + '4')
+        for 列 in range(8, 13):
+            对应字 = 表单.cell(row = 行, column = 列).value
+            if 对应字 != None:
+                所有拼音.append(拼音2 + str(列 - 8))
 
 
 遍历表格("汉语拼音四声大全.xlsx")
