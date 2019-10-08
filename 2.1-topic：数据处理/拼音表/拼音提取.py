8236868d3ffaad1d0f082fda9b296fa4f0fbcfa7
fromwheretowhere <fromwheretowhere.service@gmail.com>
2019-10-08 00:01:37 -0700

    继续简化

--- 2.1-topic：数据处理/拼音表/拼音提取.py
+++ 2.1-topic：数据处理/拼音表/拼音提取.py
@@ -9,19 +9,14 @@ def 遍历表格(文件名):
     工作簿 = load_workbook(文件名)
     表单 = 工作簿.active
     for 行 in range(开始行, 结束行):
-        拼音1 = 表单.cell(row = 行, column = 1).value.lower()
+        for 拼音序号 in range(0, 2):
+            拼音所在列 = 拼音序号 * 6 + 1
+            拼音 = 表单.cell(row = 行, column = 拼音所在列).value.lower()
 
-        for 列 in range(2, 7):
-            对应字 = 表单.cell(row = 行, column = 列).value
-            if 对应字 != None:
-                所有拼音.append(拼音1 + str(列 - 2))
-
-        拼音2 = 表单.cell(row = 行, column = 7).value.lower()
-
-        for 列 in range(8, 13):
-            对应字 = 表单.cell(row = 行, column = 列).value
-            if 对应字 != None:
-                所有拼音.append(拼音2 + str(列 - 8))
+            for 列 in range(拼音所在列 + 1, 拼音所在列 + 6):
+                对应字 = 表单.cell(row = 行, column = 列).value
+                if 对应字 != None:
+                    所有拼音.append(拼音 + str(列 - 拼音所在列 - 1))
 
 
 遍历表格("汉语拼音四声大全.xlsx")
