813556605b0481d628cb8c270cb175311b45b2e3
fromwheretowhere <fromwheretowhere.service@gmail.com>
2019-10-30 12:32:16 -0700

    改进

--- 1-基础/字图/爱心.py
+++ 1-基础/字图/爱心.py
@@ -1,11 +1,16 @@
 # 源自：https://blog.csdn.net/hitzijiyingcai/article/details/81152174
-词语 = '中文编程'
+词语 = '用中文编程'
 所有行 = []
-for 行 in range(15,-15,-1):
-    所有字段 = []
-    for x in range(-15,15):
-        x某 = x * 0.1
-        y某 = 行 * 0.1
-        所有字段.append(词语[(x - 行) % len(词语)] if (x某 ** 2 + y某 ** 2 - 1 ) ** 3 - x某 ** 2 * y某 ** 3 <= 0 else '一')
-    所有行.append(''.join(所有字段))
+for 行数 in range(30, -30, -1):
+    一行 = []
+    for 列数 in range(-30,30):
+        x某 = 列数 * 0.05
+        y某 = 行数 * 0.05
+        字 = ""
+        if (x某 ** 2 + y某 ** 2 - 1 ) ** 3 - x某 ** 2 * y某 ** 3 <= 0:
+            字 = 词语[(列数 - 行数) % len(词语)]
+        else:
+            字 = '一'
+        一行.append(字)
+    所有行.append(''.join(一行))
 print('\n'.join(所有行))
