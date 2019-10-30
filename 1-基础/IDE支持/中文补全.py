aecb2c4e82a4d97a9612fbae10d97c880a4a688c
nobodxbodon <nobodxbodon.github@gmail.com>
2019-10-30 14:56:29 -0700

    win7测试

--- 1-基础/IDE支持/中文补全.py
+++ 1-基础/IDE支持/中文补全.py
@@ -3,11 +3,11 @@ for iiiii in range(1, 10):
     print(iiiii)
 
 for 拼音 in range(1, 10):
-    # 有补全（Mac下），输入”拼“
+    # 有补全（Mac, 但win7没有），输入”拼“
     print(拼音)
 
 for 名字 in range(1, 10):
-    # 有补全（Mac下），输入”名“
+    # 有补全（Mac, 但win7没有），输入”名“
     print(名字)
 
 for 序号 in range(1, 10):
