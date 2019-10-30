9ae3317c8697f7e2532a849b568ecb5c18ac1e9f
fromwheretowhere <fromwheretowhere.service@gmail.com>
2019-10-30 14:43:37 -0700

    补全测试

---
+++ 1-基础/IDE支持/中文补全.py
@@ -0,0 +1,15 @@
+for iiiii in range(1, 10):
+    # 有补全，输入“ii"
+    print(iiiii)
+
+for 拼音 in range(1, 10):
+    # 有补全（Mac下），输入”拼“
+    print(拼音)
+
+for 名字 in range(1, 10):
+    # 有补全（Mac下），输入”名“
+    print(名字)
+
+for 序号 in range(1, 10):
+    # 没有补全！输入”序“
+    print(序号)
