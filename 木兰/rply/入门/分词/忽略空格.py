ebac41f13d8e3028b9b156bcc6dd8c14ce1b716a
fromwheretowhere <fromwheretowhere.service@gmail.com>
2020-01-29 11:35:19 -0800

    忽略

---
+++ 木兰/rply/入门/分词/忽略空格.py
@@ -0,0 +1,15 @@
+from rply import LexerGenerator
+
+分词器母机 = LexerGenerator()
+
+分词器母机.add('数', r'\d+')
+
+分词器母机.add('加', r'\+')
+分词器母机.add('减', r'-')
+
+分词器母机.ignore(r'\s+')
+
+分词器 = 分词器母机.build()
+
+for 词 in 分词器.lex('1 + 1'):
+    print(词)
