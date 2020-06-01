a959e2560c3a4cadd0d948573a3de2de75122092
fromwheretowhere <fromwheretowhere.service@gmail.com>
2020-06-01 00:58:39 -0700

    0.0.5 测试

--- 1-基础/小游戏/setup.py
+++ 1-基础/小游戏/setup.py
@@ -5,7 +5,7 @@ with open("README.md", encoding="utf-8", mode="r") as readme:
 
 setuptools.setup(
     name="demo-game-guess-number",
-    version="0.0.4",
+    version="0.0.5",
     author="无名",
     author_email="nobodxbodon.github@gmail.com",
     entry_points = {
